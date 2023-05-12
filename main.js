d6edbfa6fd258565e6686b9591a8fb5ea1f57f0e
EaZzy4K <126105844+EaZzy4K@users.noreply.github.com>
2023-05-12 22:57:26 +0400

    main.js

--- main.js
+++ main.js
@@ -1,61 +1,43 @@
 // #1
-const users = [
-    {name: 'Temo', age: 25},
-    {name: 'Lasha', age: 21},
-    {name: 'Ana', age: 28}
-]
-
-function getYoungestName(users) {
-let theYoungest = users[0]
-for (let i = 0; i < users.length; i++) {
-    if (users[i].age < theYoungest.age) {
-        theYoungest = users[i];
-    }
+function myFunction(a, b) {
+    if (a===b) {
+        return "ტოლია";
+    } else {
+        return "არ არის ტოლი";\
+     }
 }
-return theYoungest.name;
-}
-const theYoungestName = getYoungestName(users);
-console.log(theYoungestName);
+// მაგალითი:
+console.log(myFunction(2,5))
 
 // #2
-const user = {
-    name: 'Sherlock',
-    surname: 'Holmes',
-    IQ: 190
-}
+function mySecondFunction(Farenheit) {
+    const Celsius = (Farenheit-32)*5/9;
 
-function copyUser(user) {
-    const newUser = {...user};
-    return newUser;
-}
-console.log(copyUser(user))
-// #3
-let countA = 0;
-let countB = 0;
-
-let randomA;
-let randomB;
-
-const user1 = 'a';
-const user2 = 'b';
-
-function diceRandom(user1, user2) {
-    if (countA < countB) {
-        return user1 + ' არის გამარჯვებული';
-    } else if (countA > countB) {
-        return user2 + ' არის გამარჯვებული';
+    if(isNaN(Farenheit)) {
+        return false;
     } else {
-        return 'ფრე';
-    } 
-}
-
-do {
-    randomA = Math.floor(Math.random() * 6) + 1;
-    countA++;
-} while (randomA !== 3);
-
-do {
-    randomB = Math.floor(Math.random() * 6) + 1;
-    countB++;
-} while (randomB !== 3);
-console.log(diceRandom(user1, user2))
+        return Celsius;
+    }
+    console.log(mySecondFunction(60))
+    
+    // #3
+    const operators = ['+', '-', '*', '/'];
+function myThirdFunction(a, b, operation) {
+    if (isNaN(a) || isNaN(b)) {
+        return false;
+    }
+    if (operation === '+') {
+        return a+b;
+    } else if (operation === '-') {
+        return a-b;
+    } else if (operation === '*') {
+        return a*b;
+    } else if (operation === '/') {
+        return a/b;
+      } else {
+        return false;
+    }
+} 
+console.log(myThirdFunction(10,5,operators[2]))
+// სრულდება 10*5 გამრავლება და ვღებულობთ 50. 
+// P.S, operation ცვლადს გადავარქვი operators და არგუმენტად operation დავტოვე, ლოგიკაში ასე უფრო ჩაჯდა და მეკომფორტულა.
